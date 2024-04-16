ff78005f578e2236b448266814816e0b22534189
kudongku <kudongku@naver.com>
2024-04-16 16:10:52 +0900

    :tada: feat(global) : git init commit

---
+++ src/main/java/com/example/foremail/ForEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.foremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForEmailApplication.class, args);
+    }
+
+}
